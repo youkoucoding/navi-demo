13b8b59ef149056af60068222bc85c4652dcb095
youkoucoding <youkousai@gmail.com>
2021-04-12 23:06:29 +0900

    CardList & debug

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,15 +4,11 @@ import { sites } from '../data/data';
 
 const CardList = () => {
     return (
-        <>
-
-            <div className='grid grid-flow-row grid-cols-3 grid-rows-3 gap-3'>
-                {sites.map((item, id) => (
-                    <Card title={item.title}></Card>
-                ))}
-            </div>
-        </>
-
+        <div className='grid grid-flow-row grid-cols-3 grid-rows-3 gap-3'>
+            {sites.map((item, id) => (
+                <Card title={item.title}></Card>
+            ))}
+        </div>
     );
 };
 
