1e58cbd44532ff06b235a87215588896417e4ce7
youkoucoding <youkousai@gmail.com>
2021-04-14 00:19:22 +0900

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import CardList from './components/CardList';
-import SearchBar from './components/SearchBar';
+import React, { useState } from 'react';
+import Fuse from 'fuse.js';
+import { sites } from './data/data';
+import { CardList } from './components/CardList';
 
+const Search = (pattern) => {
+    const options = {
+        keys: ['element.name']
+    };
+    const fuse = new Fuse(sites, options);
+    const result = fuse.search(pattern);
+    return result;
+};
 
-function App() {
-  return (
-    <div className='flex flex-col items-center min-w-screen min-h-screen bg-hero'>
-      <SearchBar />
-      <CardList />
-    </div>
-  );
-}
+const App = () => {
+
+    const [value, setValue] = useState('');
+
+    return (
+        <div className="flex flex-col items-center min-w-screen min-h-screen bg-hero">
+            <form className='flex mt-20 mb-10'>
+                <input
+                    className='input-blue'
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    placeholder="Input What you like"
+                />
+                <button className='btn-blue'>
+                    Search
+            </button>
+            </form>
+            <CardList result={Search(value)} />
+        </div>
+    );
+};
 
 export default App;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import Card from './Card';
 import { sites } from '../data/data';
-import { result } from '../utlis/filter';
 
-const CardList = () => {
+export const CardList = ({ result }) => {
     return (
         <>
-            <div className='grid grid-flow-row grid-cols-3 sm:grid-rows-2 gap-y-4 gap-x-0'>
-                {result.map((res, index) => (
-                    <Card
-                        title={res.item.title}
-                        key={index}
-                        element={res.item.element}
-                        color={res.item.color}
-                    ></Card>
-                ))}
-            </div>
+            {
+                (result.length == 0) ? <div className='grid grid-flow-row grid-cols-3 sm:grid-rows-2 gap-y-4 gap-x-0'>
+                    {sites.map((item, index) => (
+                        <Card
+                            title={item.title}
+                            key={index}
+                            element={item.element}
+                            color={item.color}
+                        />
+                    ))}
+                </div> :
+                    <div className='grid grid-flow-row grid-cols-3 sm:grid-rows-2 gap-y-4 gap-x-0'>
+                        {result.map((res, index) => (
+                            <Card
+                                title={res.item.title}
+                                key={index}
+                                element={res.item.element}
+                                color={res.item.color}
+                            />
+                        ))}
+                    </div>
+
+            }
         </>
     );
 };
-
-export default CardList;
